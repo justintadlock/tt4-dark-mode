5755b661f684771028eaa2291396f018786db7e9
Justin Tadlock <justintadlock@gmail.com>
2025-08-28 14:26:25 -0500

    Commit last changes from previous tutorial.

--- functions.php
+++ functions.php
@@ -10,11 +10,31 @@
  * @link      https://github.com/justintadlock/tt4-dark-mode
  */
 
-// Adds an editor stylesheet.
-add_action( 'enqueue_block_editor_assets', function() {
+// Enqueue editor UI assets.
+add_action( 'enqueue_block_editor_assets', 'tt4_dark_mode_editor_assets' );
+
+function tt4_dark_mode_editor_assets() {
 	wp_enqueue_style(
 		'tt4-dark-mode-editor',
 		get_theme_file_uri( 'assets/editor.css' ),
 		filemtime( get_theme_file_path( 'assets/editor.css' ) )
 	);
-} );
+}
+
+// == The below is only needed if not adding CSS via `theme.json`. == //
+
+/*
+// Enqueue front-end assets.
+add_action( 'wp_enqueue_scripts', 'tt4_dark_mode_enqueue_assets' );
+
+function tt4_dark_mode_enqueue_assets() {
+	wp_enqueue_style( 'tt4-dark-mode', get_stylesheet_uri() );
+}
+
+// Add editor stylesheet.
+add_action( 'after_setup_theme', 'tt4_dark_mode_theme_setup' );
+
+function tt4_dark_mode_theme_setup() {
+	add_editor_style( get_stylesheet_uri() );
+}
+*/
